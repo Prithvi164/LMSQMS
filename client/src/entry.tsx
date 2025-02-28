84d54d1536f0475d484a807c10976efe6d1cc3f3
himanshu patil <39801082-himanshu2303@users.noreply.replit.com>
2025-02-28 17:46:36 +0000

    Restored to '9b7b4c57245a23ae746bf603c408989658145e7a'
    Replit-Restored-To: 9b7b4c57245a23ae746bf603c408989658145e7a

--- client/src/entry.tsx
+++ client/src/entry.tsx
@@ -1,18 +1,12 @@
 
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import { Router } from 'wouter';
 import App from './App';
-
-// Static location hook for SSR
-const staticLocationHook = (path: string) => [path, () => {}];
 
 export function render(url: string) {
   return ReactDOMServer.renderToString(
     <React.StrictMode>
-      <Router hook={() => staticLocationHook(url)} base="">
-        <App />
-      </Router>
+      <App />
     </React.StrictMode>
   );
 }
